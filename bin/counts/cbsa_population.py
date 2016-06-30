d11e58c41fc3c62f85d17daa713d5b037adb7ed7
Remi Louf <remi@sciti.es>
2016-06-30 16:19:29 +0200

    Add cbsa names in count file

--- bin/counts/cbsa_population.py
+++ bin/counts/cbsa_population.py
@@ -6,17 +6,17 @@ import csv
 
 
 ## Import the list of MSA
-msa = {}
+cbsa = {}
 with open('data/misc/cbsa_names.txt', 'r') as source:
     reader = csv.reader(source, delimiter='\t')
     reader.next()
     for rows in reader:
-        msa[rows[0]] = rows[1]
+        cbsa[rows[0]] = rows[1]
 
 
 ## Get the total number of households per MSA
 households = {}
-for city in msa:
+for city in cbsa:
     households[city]=0
     with open('data/income/cbsa/%s/blockgroups.txt'%city, 'r') as source:
         reader = csv.reader(source, delimiter='\t')
@@ -27,6 +27,6 @@ for city in msa:
 
 ## Save the data
 with open('data/counts/cbsa.txt', 'w') as output:
-    output.write('CBSA FIP\tNumber of households\n')
+    output.write('CBSA FIP\tName\tNumber of households\n')
     for city in households:
-        output.write('%s\t%s\n'%(city, households[city]))
+        output.write('%s\t%s\t%s\n'%(city, cbsa[city], households[city]))
